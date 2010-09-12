312b6abd46c006bed251daca35f232f5dab88d02
John Grimes <me@johngrimes.info>
2010-09-12 23:12:11 +1000

    Minor revisions to view generators

--- lib/generators/erb/t_minus_generator.rb
+++ lib/generators/erb/t_minus_generator.rb
@@ -1,12 +1,15 @@
 module Erb
   module Generators
     class TMinusGenerator < Rails::Generators::Base
+      desc 'Generates ERB view template for T-Minus'
+      source_root File.expand_path('../templates', __FILE__)
+
       def create_prelaunch_directory
         empty_directory 'app/views/prelaunch'
       end
 
       def copy_view_file
-        template 'new.html.erb', 'app/views/prelaunch/new.html.erb'
+        copy_file 'new.html.erb', 'app/views/prelaunch/new.html.erb'
       end
     end
   end
--- lib/generators/haml/t_minus_generator.rb
+++ lib/generators/haml/t_minus_generator.rb
@@ -1,12 +1,15 @@
 module Haml
   module Generators
     class TMinusGenerator < Rails::Generators::Base
+      desc 'Generates Haml view template for T-Minus'
+      source_root File.expand_path('../templates', __FILE__)
+
       def create_prelaunch_directory
         empty_directory 'app/views/prelaunch'
       end
 
       def copy_view_file
-        template 'new.html.haml', 'app/views/prelaunch/new.html.haml'
+        copy_file 'new.html.haml', 'app/views/prelaunch/new.html.haml'
       end
     end
   end
