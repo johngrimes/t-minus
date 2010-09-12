172311bad478205112bc9aff441021dcbdb58f6d
John Grimes <me@johngrimes.info>
2010-09-12 18:04:39 +1000

    Removing active record generator

--- lib/generators/tee_minus/install_generator.rb
+++ lib/generators/tee_minus/install_generator.rb
@@ -1,15 +1,18 @@
 module TeeMinus
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      desc 'Generates configuration and migration files for TeeMinus'
+      desc 'Generates configuration and migration files for Tee Minus'
 
       source_root File.expand_path('../templates', __FILE__)
-
-      hook_for :orm
 
       def copy_configuration_file
         copy_file 'prelaunch_config.yml', 'config/prelaunch_config.yml'
       end
+
+      def generate_migration
+        migration_template 'migration.rb', "db/migrate/create_prelaunch_subscribers"
+      end
+
     end
   end
 end
