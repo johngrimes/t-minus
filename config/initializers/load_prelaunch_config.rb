6a173a2a5bd32774b5fbe1b6e74d62062bc405e2
John Grimes <me@johngrimes.info>
2010-09-13 21:42:23 +1000

    Initialising variable for Campaigning

--- config/initializers/load_prelaunch_config.rb
+++ config/initializers/load_prelaunch_config.rb
@@ -1,2 +1,4 @@
 raw_config = File.read("#{Rails.root}/config/prelaunch_config.yml")
 PRELAUNCH_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
+
+CAMPAIGN_MONITOR_API_KEY = PRELAUNCH_CONFIG[:campaign_monitor_api_key]
