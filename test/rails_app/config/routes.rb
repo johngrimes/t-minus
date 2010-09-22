0c7a5a615bf89ddec8c1bd312ccff17f5ba78cc4
John Grimes <me@johngrimes.info>
2010-09-23 09:13:35 +1000

    Adding scaffold files to test project

--- test/rails_app/config/routes.rb
+++ test/rails_app/config/routes.rb
@@ -1,4 +1,6 @@
 RailsApp::Application.routes.draw do
+  resources :articles
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- test/rails_app/db/schema.rb
+++ test/rails_app/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100919093846) do
+ActiveRecord::Schema.define(:version => 20100922070312) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "prelaunch_subscribers", :force => true do |t|
     t.string   "email"
